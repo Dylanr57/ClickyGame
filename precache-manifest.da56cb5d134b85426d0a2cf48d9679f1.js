29119f3d367f684f0c302f2de2cd3c3868c1218a
Dylanr57 <dylanr574423@gmail.com>
2019-01-31 19:15:34 -0800

    Updates

---
+++ precache-manifest.da56cb5d134b85426d0a2cf48d9679f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "59843ae1c2ed89599fd0",
+    "url": "/ClickyGame/static/js/runtime~main.59843ae1.js"
+  },
+  {
+    "revision": "c63b29231fceb5037ff9",
+    "url": "/ClickyGame/static/js/main.c63b2923.chunk.js"
+  },
+  {
+    "revision": "6dc4eabbb4e2b85efd10",
+    "url": "/ClickyGame/static/js/1.6dc4eabb.chunk.js"
+  },
+  {
+    "revision": "c63b29231fceb5037ff9",
+    "url": "/ClickyGame/static/css/main.c18f0802.chunk.css"
+  },
+  {
+    "revision": "d0d8b38fc773b6915793cc4ce50102fb",
+    "url": "/ClickyGame/index.html"
+  }
+];
